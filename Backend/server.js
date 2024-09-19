d42790118c0703876b146a283aead0aab27d137b
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-09-19 12:17:06 +0530

    dependancies

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+app.get("/", (req, res) => {
+  //root route http://localhost:5000
+  res.send("hello world 1111");
+});
 app.listen(5000, () => {
-  console.log("server running at port 5000");
+  console.log("server running at port 5000 ..111..");
 });
