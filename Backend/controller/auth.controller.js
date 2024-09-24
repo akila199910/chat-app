138c29dc5905757f85aeb3afc510bd61cfac10c1
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-09-24 13:25:29 +0530

    create login and logout functions

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -48,10 +48,38 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  console.log("login user");
+export const login = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      userName: user.userName,
+      gender: user.gender,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const logout = (req, res) => {
-  console.log("logout user");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
