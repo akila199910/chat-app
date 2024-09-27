bba2e30db32d1f9b07baf65c7ef1e05c145ef98f
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-09-27 16:16:11 +0530

    cretae user function

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/Message.route.js";
+import userRoutes from "./routes/User.route.js";
 
 import connectToDb from "./db/connectToDb.js";
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToDb();
