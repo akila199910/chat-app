86c2058358d7ffa06c24efe86f37ac0c49c88367
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-09-27 12:42:15 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/Message.route.js";
+
 import connectToDb from "./db/connectToDb.js";
 
 const app = express();
@@ -10,8 +13,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToDb();
