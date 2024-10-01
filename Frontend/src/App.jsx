81ef95cb09490e49661fb3f80a78db63f4372acf
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-01 10:41:38 +0530

    create signup form

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 
-import Login from "./Pages/login/Login";
+// import Login from "./Pages/login/Login";
+import Signup from "./Pages/signup/Signup";
 
 function App() {
   return (
     <div className=" p-4 h-screen flex items-center justify-center">
-      <Login />
+      <Signup/>
     </div>
   );
 }
