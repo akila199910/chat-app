d826e5079b1dde5b2e7d0ad966a823c27b1edfa4
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-02 17:48:18 +0530

    connect backend frontend  signup  function and auth context create

--- Frontend/src/hook/useSignup.js
+++ Frontend/src/hook/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
   const signup = async ({
     fullName,
     userName,
@@ -35,6 +37,11 @@ const useSignup = () => {
       });
 
       const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      setAuthUser(data);
       console.log(data);
     } catch (error) {
       toast.error(error.message);
