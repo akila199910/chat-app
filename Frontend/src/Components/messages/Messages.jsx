9be135d763ab113ea0e0cfce419577991e9ae2f2
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-03 18:02:35 +0530

    create convesation

--- Frontend/src/Components/messages/Messages.jsx
+++ Frontend/src/Components/messages/Messages.jsx
@@ -1,20 +1,16 @@
+import useGetMessages from "../../hook/useGetMessages";
+import MessageSkeleton from "../skeleton/messageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
+  const {messages, loading}= useGetMessages();
   return (
     <div className=" px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-
+          {!loading && messages.length > 0 && messages.map((message) => <Message key={message._id} message={message} />) }
+      {loading && [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
     </div>
   );
 };
