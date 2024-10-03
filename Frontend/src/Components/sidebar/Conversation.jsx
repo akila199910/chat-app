9e90ef4670d8947804bdbefa05e57440b375e9c9
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-03 13:43:02 +0530

    fetch the all the user for conversations

--- Frontend/src/Components/sidebar/Conversation.jsx
+++ Frontend/src/Components/sidebar/Conversation.jsx
@@ -1,30 +1,25 @@
-const Conversation = () => {
+const Conversation = ({ conversation, emoji, lastindex }) => {
   return (
     <>
       <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
         <div className="avatar online">
           <div className=" w-12 rounded-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              alt="user avatar"
-            />
+            <img src={conversation.profilePic} alt="user avatar" />
           </div>
         </div>
         <div className="flex flex-col flex-1">
-            <div className=" flex gap-3 justify-between">
-                <p className=" font-bold text-gray-200"></p>
-                <span className=" text-xl ">imo</span>
-            </div>
+          <div className=" flex gap-3 justify-between">
+            <p className=" font-bold text-gray-200">{conversation.fullName}</p>
+            <span className=" text-xl ">{emoji}</span>
+          </div>
         </div>
       </div>
-
-      <div className=" divider my-0 py-0 h-1"/>
+      {!lastindex && <div className=" divider my-0 py-0 h-1" />}
     </>
   );
 };
 
 export default Conversation;
-
 
 // const Conversation = () => {
 //     return (
@@ -45,11 +40,10 @@ export default Conversation;
 //               </div>
 //           </div>
 //         </div>
-  
+
 //         <div className=" divider my-0 py-0 h-1"/>
 //       </>
 //     );
 //   };
-  
+
 //   export default Conversation;
-  
--- Frontend/src/Components/sidebar/Conversations.jsx
+++ Frontend/src/Components/sidebar/Conversations.jsx
@@ -1,19 +1,29 @@
+import useGetConversations from "../../hook/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+  const { loading, conversation } = useGetConversations();
+
   return (
     <div className=" py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {loading ? (
+        <div className="loading loading-spinner"></div>
+      ) : (
+        conversation.map((conversation, index) => (
+          <Conversation
+            key={conversation._id}
+            conversation={conversation}
+            emoji={getRandomEmoji()}
+            lastindex={index === conversation.length - 1}
+          />
+        ))
+      )}
     </div>
   );
 };
 
 export default Conversations;
-
 
 // import Conversation from "./Conversation";
 
