e2fa7b64621f308d736012548e4a626104888d05
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-03 16:38:26 +0530

    front end send message function crete

--- Frontend/src/Components/messages/MessageInput.jsx
+++ Frontend/src/Components/messages/MessageInput.jsx
@@ -1,18 +1,38 @@
+import { useState } from "react";
 import { BsFillSendFill } from "react-icons/bs";
+import useSendMessage from "../../hook/useSendMessage";
 const MessageInput = () => {
+  const [message, setMessage] = useState("");
+  const { loading, sendMessage } = useSendMessage();
+  const handleMessageFormSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!message) {
+      return;
+    }
+
+    await sendMessage(message);
+    setMessage("");
+  };
   return (
-    <form className=" px-4 my-3">
+    <form className=" px-4 my-3" onSubmit={handleMessageFormSubmit}>
       <div className="w-full relative">
         <input
           type="text"
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
           placeholder="Type a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center pe-3"
         >
-          <BsFillSendFill />
+          {loading ? (
+            <div className=" loading loading-spinner"></div>
+          ) : (
+            <BsFillSendFill />
+          )}
         </button>
       </div>
     </form>
@@ -20,7 +40,6 @@ const MessageInput = () => {
 };
 
 export default MessageInput;
-
 
 // import { BsFillSendFill } from "react-icons/bs";
 // const MessageInput = () => {
