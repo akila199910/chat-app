1179336f5e867626d784f73c246a0184b4e05cf9
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-03 11:32:10 +0530

    login function create

--- Frontend/src/Pages/login/Login.jsx
+++ Frontend/src/Pages/login/Login.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../../hook/useLogin";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { loading, login } = useLogin();
+  const handleLoginFormSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
   return (
     <div className=" flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className=" w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -8,7 +18,7 @@ const Login = () => {
           Login <span className=" text-blue-500">ChatApp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleLoginFormSubmit}>
           <div>
             <label className=" label p-2">
               <span className=" text-base label-text">Username</span>
@@ -17,6 +27,8 @@ const Login = () => {
               type="text"
               placeholder="Enter username"
               className=" w-full input input-bordered h-10"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
             />
           </div>
           <div>
@@ -27,6 +39,8 @@ const Login = () => {
               type="password"
               placeholder="Enter password"
               className=" w-full input input-bordered h-10"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
           <Link
@@ -36,7 +50,13 @@ const Login = () => {
             {"Don't"} have an account?
           </Link>
           <div>
-            <button className=" btn btn-block btn-sm mt-2">Login</button>
+            <button className=" btn btn-block btn-sm mt-2" disabled={loading}>
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
+            </button>
           </div>
         </form>
       </div>
