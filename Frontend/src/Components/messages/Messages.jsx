564aea149d1f6618d60387f56dc56c241cbe3e92
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-04 09:05:28 +0530

    customize the conversation with time

--- Frontend/src/Components/messages/Messages.jsx
+++ Frontend/src/Components/messages/Messages.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useRef } from "react";
 import useGetMessages from "../../hook/useGetMessages";
 import MessageSkeleton from "../skeleton/messageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
-  const {messages, loading}= useGetMessages();
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
   return (
     <div className=" px-4 flex-1 overflow-auto">
-          {!loading && messages.length > 0 && messages.map((message) => <Message key={message._id} message={message} />) }
-      {loading && [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
+        ))}
+      {loading &&
+        [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
       {!loading && messages.length === 0 && (
         <p className="text-center">Send a message to start the conversation</p>
       )}
