73783a18a5bd6c747397f759dadfa4757e6f947f
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-05 11:20:11 +0530

    configarations of socket io

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import { app, server } from "./socket/socket.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/Message.route.js";
 import userRoutes from "./routes/User.route.js";
 
 import connectToDb from "./db/connectToDb.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -20,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToDb();
   console.log(`server running at port ${PORT}`);
 });
