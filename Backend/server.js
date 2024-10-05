226bd1bf0587fede54239b7004a955bc12795f3a
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-05 12:07:31 +0530

    create SocketContext

--- Backend/server.js
+++ Backend/server.js
@@ -11,8 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(express.json());
+// app.use(express.json());
 app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
